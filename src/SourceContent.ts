52341d730e09d9033cab5b112ca340cee356f21e
Alan S. Ferreira <alansferreira1984@gmail.com>
2024-08-06 22:59:04 -0300

    feat!: upgrade get / set contents to use uri standards

--- src/SourceContent.ts
+++ src/SourceContent.ts
@@ -1,35 +1,211 @@
 import { getVariable, setVariable } from "azure-pipelines-task-lib";
-import { _warning } from "azure-pipelines-task-lib/internal";
+import { _error, _warning } from "azure-pipelines-task-lib/internal";
 import { readFileSync, writeFileSync } from "fs";
+import path = require("path");
+import axios from "axios";
 
-export type SourceType = 'file' | 'text' | 'var';
-export type DestType = 'file' | 'echo' | 'var' | 'out' | 'secret';
-export const getContentHandles: Record<SourceType, (source: string) => Promise<string>> = {
-  'file': (file) => Promise.resolve(readFileSync(file).toString('utf-8')),
-  'var': (varname) => Promise.resolve(getVariable(varname) ?? ''),
-  'text': (content) => Promise.resolve(content)
-}
+/**
+ * @deprecated use {@link GetProtocols}
+ */
+export type SourceType = "file" | "text" | "var";
+export type GetProtocols = "var" | "file" | "http" | "https";
 
-export const setContentHandles: Record<DestType, (dest: string, content: Buffer | string) => Promise<void>> = {
-  'file': (file, content) => Promise.resolve(writeFileSync(file, content)),
-  'var': (varname, value) => Promise.resolve(setVariable(varname, value.toString('utf-8'))),
-  'out': (varname, value) => Promise.resolve(setVariable(varname, value.toString('utf-8'), false, true)),
-  'secret': (varname, value) => Promise.resolve(setVariable(varname, value.toString('utf-8'), true, false)),
-  'echo': (_, content) => Promise.resolve(console.log(content.toString('utf-8'))),
-}
+/**
+ * @deprecated use {@link SetProtocols}
+ */
+export type DestType = "file" | "echo" | "var" | "out" | "secret";
+export type SetProtocols =
+  | "file"
+  | "echo"
+  | "var"
+  | "out"
+  | "secret"
+  | "http"
+  | "https";
 
+/**
+ * @deprecated use {@link getHandles}
+ */
+export const getContentHandles: Record<
+  SourceType,
+  (source: string) => Promise<string>
+> = {
+  file: (file) => Promise.resolve(readFileSync(file).toString("utf-8")),
+  var: (varname) => Promise.resolve(getVariable(varname) ?? ""),
+  text: (content) => Promise.resolve(content),
+};
+
+export const getHandles: Record<
+  GetProtocols,
+  (sourceUri: string, parsedUrl: URL) => Promise<string>
+> = {
+  file: (file, parsed) =>
+    Promise.resolve(
+      readFileSync(path.join(parsed.hostname, parsed.pathname)).toString(
+        "utf-8"
+      )
+    ),
+  var: (varname, parsed) => Promise.resolve(getVariable(parsed.hostname) ?? ""),
+  http: async (url, parsed) => {
+    const res = await axios.get(url, { timeout: 1000 });
+    return res.data;
+  },
+  https: async (url, parsed) => {
+    const res = await axios.get(url, { timeout: 1000 });
+    return res.data;
+  },
+};
+
+/**
+ * @deprecated @see {@link setHandles}
+ */
+export const setContentHandles: Record<
+  DestType,
+  (dest: string, content: Buffer | string) => Promise<void>
+> = {
+  file: (file, content) => Promise.resolve(writeFileSync(file, content)),
+  var: (varname, value) =>
+    Promise.resolve(setVariable(varname, value.toString("utf-8"))),
+  out: (varname, value) =>
+    Promise.resolve(setVariable(varname, value.toString("utf-8"), false, true)),
+  secret: (varname, value) =>
+    Promise.resolve(setVariable(varname, value.toString("utf-8"), true, false)),
+  echo: (_, content) => Promise.resolve(console.log(content.toString("utf-8"))),
+};
+
+export const setHandles: Record<
+  SetProtocols,
+  (
+    dest: string,
+    parsedUrl: URL | undefined,
+    content: Buffer | string
+  ) => Promise<void>
+> = {
+  file: (file, parsedUrl, content) =>
+    Promise.resolve(writeFileSync(file, content)),
+  var: (varname, parsedUrl, value) =>
+    Promise.resolve(setVariable(varname, value.toString("utf-8"))),
+  out: (varname, parsedUrl, value) =>
+    Promise.resolve(setVariable(varname, value.toString("utf-8"), false, true)),
+  secret: (varname, parsedUrl, value) =>
+    Promise.resolve(setVariable(varname, value.toString("utf-8"), true, false)),
+  echo: (_, parsedUrl, content) =>
+    Promise.resolve(console.log(content.toString("utf-8"))),
+  http: async (url, parsed) => {
+    const res = await axios.post(url);
+    return res.data;
+  },
+  https: async (url, parsed) => {
+    const res = await axios.post(url);
+    return res.data;
+  },
+};
+
+/**
+ * @deprecated @see {@link get}
+ * @param sourceType
+ * @param source
+ * @returns
+ */
 export const getContent = (sourceType: SourceType, source: string) => {
-  const handle = getContentHandles[sourceType] ?? getContentHandles.text
-  if(getContent == getContentHandles.text && sourceType != 'text') {
-    _warning(`Source Type '${sourceType}' is not implemented, using default 'text'.`);
+  const handle = getContentHandles[sourceType] ?? getContentHandles.text;
+  if (handle == getContentHandles.text && sourceType != "text") {
+    _warning(
+      `Source Type '${sourceType}' is not implemented, using default 'text'.`
+    );
   }
   return handle(source);
-}
+};
 
-export const setContent = async (destType: DestType, dest: string, content: Buffer | string) => {
-  const handle = setContentHandles[destType] ?? getContentHandles.text
-  if(setContent == setContentHandles.echo && destType != 'echo') {
-    _warning(`Source Type '${destType}' is not implemented, using default 'text'.`);
+/**
+ *
+ * @param sourceUri
+ * **uri based source** target or **raw string**
+ *
+ * [**var** | **file** | **http**]://target
+ *
+ * `var://MY_VAR_NAME`
+ *
+ * `file:///path/pf/file.txt`
+ *
+ * `http://host:port/my-get-route-path...`
+ *
+ * `my raw any string without protocol reference`
+ * @returns content from sourceUri
+ */
+export const get = async (sourceUri: string): Promise<string> => {
+  try {
+    const parsedUri = new URL(sourceUri);
+    const protocol = parsedUri.protocol.slice(0, -1);
+
+    const handle = getHandles[protocol];
+    if (!handle) {
+      _warning(
+        `Source Type '${protocol}' is not implemented, using default 'text', IF YOU REALY WANT TO USE RAW TEXT DO NOT USE URI sintax.`
+      );
+      return Promise.resolve(sourceUri);
+    }
+
+    return await handle(sourceUri, parsedUri);
+  } catch (error) {
+    _error(error?.data ?? error);
+    return await Promise.resolve(sourceUri);
   }
-  handle(dest, content);
-}
+};
+
+/**
+ * @deprecated
+ * @param destType
+ * @param dest
+ * @param content
+ */
+export const setContent = async (
+  destType: DestType,
+  dest: string,
+  content: Buffer | string
+) => {
+  const handle = setContentHandles[destType] ?? getContentHandles.text;
+  if (setContent == setContentHandles.echo && destType != "echo") {
+    _warning(
+      `Destination Type '${destType}' is not implemented, using default 'text'.`
+    );
+  }
+  await handle(dest, content);
+};
+
+/**
+ *
+ * @param targetUri
+ * **uri based** of target or **raw string**
+ *
+ * [ **file** | **echo** | **var** | **out** | **secret** | **http** | **https** ]://target
+ *
+ * `var://MY_VAR_NAME`
+ * `out://MY_VAR_NAME`
+ * `secret://MY_VAR_NAME`
+ *
+ * `file:///path/pf/file.txt`
+ *
+ * `http[s]://host:port/my-get-route-path...`
+ *
+ * @returns
+ */
+export const set = async (targetUri: string, content: Buffer | string) => {
+  try {
+    const parsedUri = new URL(targetUri);
+    const protocol = parsedUri.protocol.slice(0, -1);
+
+    const handle = setHandles[protocol];
+    if (!handle) {
+      _warning(
+        `Target Type '${protocol}' is not implemented, using default 'echo', IF YOU REALY WANT TO USE RAW TEXT DO NOT USE URI sintax.`
+      );
+      await setHandles.echo(targetUri, parsedUri, content);
+    }
+
+    await handle(parsedUri, content);
+  } catch (error) {
+    _error(error?.data ?? error);
+    await setHandles.echo(targetUri, undefined, content);
+  }
+};
--- src/tests/_suite.ts
+++ src/tests/_suite.ts
@@ -2,49 +2,57 @@ import * as assert from "assert";
 import { getRuntimePath } from "../RuntimeUtil";
 import { executeScript, parseScript } from "../InlineScripts";
 import { json } from "stream/consumers";
+import { get } from "../SourceContent";
 
 describe(`ParamsUtils Suite`, () => {
-  it('parseInput', async () => {
-    assert(''.length == 0, 'should have empty string')
-  })
-})
+  it("parseInput", async () => {
+    assert("".length == 0, "should have empty string");
+  });
+});
 
-describe("RuntimeUtils Suite", ()=>{
-  after(() =>{
-    delete process.env.EXT
-  })
+describe("RuntimeUtils Suite", () => {
+  after(() => {
+    delete process.env.EXT;
+  });
 
-  it('getRuntimePath', async () => {
-    process.env.EXT = 'ts';
-    const runtime = getRuntimePath('');
-    assert(runtime, 'runtime must be defined');
-    assert(runtime.endsWith('node_modules/.bin/ts-node'), 'runtime must be "ts-node".')
-  })
-})
+  it("getRuntimePath", async () => {
+    process.env.EXT = "ts";
+    const runtime = getRuntimePath("");
+    assert(runtime, "runtime must be defined");
+    assert(
+      runtime.endsWith("node_modules/.bin/ts-node"),
+      'runtime must be "ts-node".'
+    );
+  });
+});
 
-describe("InlineScripts Suite", ()=>{
-  after(() =>{
-    delete process.env.EXT
-  })
+describe("InlineScripts Suite", () => {
+  after(() => {
+    delete process.env.EXT;
+  });
 
-  it('parseScript', async () => {
-    process.env.EXT = 'ts';
-    const runtime = getRuntimePath('');
-    const queries = parseScript([
-      `var NAME    =      .metadata.name | downcase`,
-      `var KIND    =    .kind`,
-      `echo      .kind | uppercase`,
-      `file  ./bar/annotations.json  = .metadata.annotations`,
-      `out outTest   =   .emptyResultTest | uppercase`,
-      `secret secretTest =     .emptyResultTest | uppercase`,
-      `var DOC_TWO_NAME    =      $[1].metadata.name | downcase`,
-    ].join('\n'));
+  it("parseScript", async () => {
+    process.env.EXT = "ts";
+    const runtime = getRuntimePath("");
+    const queries = parseScript(
+      [
+        `var NAME    =      .metadata.name | downcase`,
+        `var KIND    =    .kind`,
+        `echo      .kind | uppercase`,
+        `file  ./bar/annotations.json  = .metadata.annotations`,
+        `out outTest   =   .emptyResultTest | uppercase`,
+        `secret secretTest =     .emptyResultTest | uppercase`,
+        `var DOC_TWO_NAME    =      $[1].metadata.name | downcase`,
+      ].join("\n")
+    );
 
-    assert(queries, 'queries must be defined');
-    assert(queries.length === 7, 'queries must have 7 items.');
-    assert(JSON.stringify(queries) === '[{"kind":"var","dest":"NAME","script":".metadata.name | downcase"},{"kind":"var","dest":"KIND","script":".kind"},{"kind":"echo","dest":"","script":".kind | uppercase"},{"kind":"file","dest":"./bar/annotations.json","script":".metadata.annotations"},{"kind":"out","dest":"outTest","script":".emptyResultTest | uppercase"},{"kind":"secret","dest":"secretTest","script":".emptyResultTest | uppercase"},{"kind":"var","dest":"DOC_TWO_NAME","script":"$[1].metadata.name | downcase"}]');
-
-  })
+    assert(queries, "queries must be defined");
+    assert(queries.length === 7, "queries must have 7 items.");
+    assert(
+      JSON.stringify(queries) ===
+        '[{"kind":"var","dest":"NAME","script":".metadata.name | downcase"},{"kind":"var","dest":"KIND","script":".kind"},{"kind":"echo","dest":"","script":".kind | uppercase"},{"kind":"file","dest":"./bar/annotations.json","script":".metadata.annotations"},{"kind":"out","dest":"outTest","script":".emptyResultTest | uppercase"},{"kind":"secret","dest":"secretTest","script":".emptyResultTest | uppercase"},{"kind":"var","dest":"DOC_TWO_NAME","script":"$[1].metadata.name | downcase"}]'
+    );
+  });
   // it('executeScript', async () => {
   //   process.env.EXT = 'ts';
   //   const runtime = getRuntimePath('');
@@ -65,5 +73,17 @@ describe("InlineScripts Suite", ()=>{
   //   });
   //   assert(counter === queries.length)
   // })
+});
+describe("SourceContent Suite", () => {
+  it("get", async () => {
+    let result: string = "";
 
-})
+    result = await get("var://MY_VAR_NAME");
+
+    result = await get("file:///path/to/file.txt");
+
+    result = await get("http://host:8080/my-get-route-path...");
+
+    result = await get("my raw any string without protocol reference");
+  });
+});
